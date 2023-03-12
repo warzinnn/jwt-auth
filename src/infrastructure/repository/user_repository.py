3abac338042dc7bd1812b103adda1b98b02beaa0
warzinnn <vkimura1@gmail.com>
2023-03-12 20:31:36 -0300

    update infra && add interface for repository

--- src/infrastructure/repository/user_repository.py
+++ src/infrastructure/repository/user_repository.py
@@ -1,13 +1,19 @@
 from typing import List
 
 from src.domain.model.user import User
+from src.infrastructure.interfaces.user_repository_interface import UserInterface
 
 
-class UserRepository:
+class UserRepository(UserInterface):
     def __init__(self, connection_handler) -> None:
         self.__connection_handler = connection_handler
 
     def select_all_users(self) -> List[User]:
+        """
+        Select data in Users table
+        :param - None
+        :return - List with all Users
+        """
         with self.__connection_handler() as db:
             try:
                 data = db.session.query(User).all()
@@ -16,6 +22,11 @@ class UserRepository:
                 return e
 
     def select_user(self, email: str) -> User:
+        """
+        Select data in Users table with filter by email
+        :param - email
+        :return - User object
+        """
         with self.__connection_handler() as db:
             try:
                 data = db.session.query(User).filter(User.email == email).scalar()
