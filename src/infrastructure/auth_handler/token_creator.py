638caf069190b5cf788d0c20ddf70abbbd74ca26
warzinnn <vkimura1@gmail.com>
2023-03-16 16:10:08 -0300

    move auth_handler to infra folder

---
+++ src/infrastructure/auth_handler/token_creator.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timedelta
+
+import jwt
+from cryptography.hazmat.primitives import serialization
+from dotenv import dotenv_values
+
+from src.interfaces.token_creator_interface import TokenCreatorInterface
+
+
+class TokenCreator(TokenCreatorInterface):
+    def __init__(self) -> None:
+        self.__TOKEN_KEY = self._get_private_key()
+        self.__EXP_TIME_MINUTES = 10
+        # self.__REFRESH_TIME_MINUTES = 5
+
+    def get_token(self, username, role):
+        """Function to return JWT token"""
+        return self._encode_token(username, role)
+
+    def _get_private_key(self):
+        """Function to get the private key"""
+        with open(".ssh/id_rsa", "r") as file:
+            private_key = file.read()
+
+        passwd = dotenv_values(".env")["KEY_PASS"]
+        key = serialization.load_ssh_private_key(
+            private_key.encode(), password=passwd.encode()
+        )
+        return key
+
+    def _encode_token(self, username, role):
+        """Function to encode the token with a payload
+        :params - None
+        :return - JWT token
+        Explanation
+        ===========
+        Payload_data
+        :params - sub: user id
+                - iat: issued at (identifies the time at which the JWT was issued)
+                - exp: expiration time (identifies the expiration for the token)
+                - role: role of the user
+        """
+        payload_data = {
+            "sub": username,
+            "iat": datetime.utcnow(),
+            "exp": datetime.utcnow() + timedelta(minutes=self.__EXP_TIME_MINUTES),
+            "role": role,
+        }
+        token = jwt.encode(
+            payload=payload_data, key=self.__TOKEN_KEY, algorithm="RS256"
+        )
+        return token
