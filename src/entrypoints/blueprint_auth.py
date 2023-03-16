969577dab3b91dd5ede1c425e7424818dca128ca
warzinnn <vkimura1@gmail.com>
2023-03-16 16:10:35 -0300

    ajust entrypoint blueprint

--- src/entrypoints/blueprint_auth.py
+++ src/entrypoints/blueprint_auth.py
@@ -1,11 +1,11 @@
-import bcrypt
 from flask import Blueprint, jsonify, request
 
-from src.auth_handler.role_verifier import role_verify
-from src.auth_handler.token_creator import TokenCreator
-from src.auth_handler.token_verifier import token_verify
+from src.infrastructure.auth_handler.role_verifier import role_verify
+from src.infrastructure.auth_handler.token_creator import TokenCreator
+from src.infrastructure.auth_handler.token_verifier import token_verify
 from src.infrastructure.config.connection import DBConnectionHandler
 from src.infrastructure.repository.user_repository import UserRepository
+from src.use_cases.authenticate_user import AuthenticateUser
 
 blueprint_auth = Blueprint(name="blueprint_auth", import_name=__name__)
 
@@ -13,22 +13,19 @@ blueprint_auth = Blueprint(name="blueprint_auth", import_name=__name__)
 @blueprint_auth.route("/auth/login", methods=["POST"])
 def do_login():
     user_repo = UserRepository(DBConnectionHandler)
+    token_creator = TokenCreator()
+    uc = AuthenticateUser()
 
     json_input = request.json
     email_input = json_input["email"]
     passwd_input = json_input["password"]
 
-    # Query database by email
-    data = user_repo.select_user(email_input)
+    # call use_case
+    result = uc.authenticate_use_case(
+        user_repo, token_creator, email_input, passwd_input
+    )
 
-    # Check password
-    password = passwd_input.encode("utf-8")
-    if bcrypt.checkpw(password, data.passwd.encode("utf-8")):
-        token_creator = TokenCreator()
-        token = token_creator.get_token(data.username, data.role)
-        return jsonify({"message": "authenticated", "token": token})
-
-    return jsonify({"message": "error"})
+    return jsonify(result)
 
 
 @blueprint_auth.route("/secret/admin", methods=["GET"])
